38ce13c7b77f8df3f3baa16dd49f81249de04644
uncenter <me@uncenter.org>
2023-06-09 19:16:21 -0400

    refactor: remove useless sign in button, change title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,9 @@ import { button } from "~/styles";
 export default function Header() {
     return (
         <div class="flex flex-row justify-between">
-            <div class="text-4xl font-extrabold dark:text-white">Pomofocus</div>
+            <div class="text-4xl font-extrabold dark:text-white">Pomodoro</div>
             <div class="flex flex-col sm:flex-row">
                 <button class={button.primary}>Settings</button>
-                <button class={button.primary}>Sign In</button>
             </div>
         </div>
     );
