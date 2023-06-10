05638173748bf82c9ef1c28439928d33b3de9449
uncenter <me@uncenter.org>
2023-06-09 21:05:47 -0400

    refactor: remove heading content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,3 @@
-import { button } from "~/styles";
-
 export default function Header() {
-    return (
-        <div class="flex flex-row justify-between">
-            <div class="text-4xl font-extrabold dark:text-white">Pomodoro</div>
-            <div class="flex flex-col sm:flex-row">
-                <button class={button.primary}>Settings</button>
-            </div>
-        </div>
-    );
+    return <div class="flex flex-row justify-between"></div>;
 }
